80c8cb35582f8949b4b51590d7e6fd672f7f4e39
Iamshayankarami <ftfsk8203@gmail.com>
2020-08-03 00:20:17 +0430

    make it bether by LOVE

--- main.py
+++ main.py
@@ -4,16 +4,35 @@ import sqlite3 as sql
 app = Flask(__name__)
 
 
-def print_data(c,search):
-	for row in c.execute(f"SELECT * FROM {search}"):
-		return row
+def print_pass_ips():
+	return_data = []
+	send = []
+	with sql.connect('test1.db') as co:
+		c = co.cursor()
+		c.execute('SELECT name FROM sqlite_master WHERE type="table";')
+		for name in c.fetchall():
+			name = name[0]
+			return_data.append([row for row in c.execute(f"SELECT * FROM {name}")])
+	for data in return_data:
+		data = data[0]
+		send.append(data[1])
+	return send
 
-def check_mac_add(mac_add):
-	mac_ad
+def _check_ip(ip, lisT):
+	output = []
+	for IP in lisT:
+		if IP == ip:
+			output.append(IP)
+	if len(output) >= 1:
+		print('old user')
+	else:
+		pass
 
 @app.route('/', methods=["GET", "POST"])
 def index():
 	ip_add = request.remote_addr
+	check_ip = print_pass_ips()
+	_check_ip(ip_add, check_ip)
 	if request.method == "POST":
 		name = request.form["name"]
 		work = request.form["work"]
@@ -24,13 +43,10 @@ def index():
 			c = conn.cursor()
 			c.execute(f'CREATE TABLE {name} (name, ip, work, timeline, timerange)')
 			c.executemany(f"INSERT INTO {name} VALUES (?, ?, ?, ?, ?)", data)
-			c.execute('SELECT name FROM sqlite_master WHERE type="table";')
-			for names in c.fetchall():
-				names = names[0]
-				print([row for row in c.execute(f"SELECT * FROM {names}")])
 			conn.commit()
 	return "<form method='POST'><p>name: </p><input type='text' name='name' id='name'><br><p>work: </p><input type='text' name='work' id='work'<br><p>timeline: </p><input type='text' name='timeline' id='timeline'><br><input type='text' name='timerange' id='timerange'><br><input type='submit' value='Submit'></form> <p>{{data}}</p>"	
 	conn.close()
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run('0.0.0.0', debug=True)
+
