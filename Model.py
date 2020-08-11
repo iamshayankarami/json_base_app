74270c0c307edf3241b415beef820243509da321
Iamshayankarami <ftfsk8203@gmail.com>
2020-08-11 16:19:03 +0430

    change to json for saving ans using data

--- Model.py
+++ Model.py
@@ -1,52 +1,59 @@
 import time
-import sqlite3 as sql
+import json
 import os
 
 
 def get_time():
 	return time.asctime()[11:19]
 
+def today_date():
+	return time.asctime()
 
 def __check_num(Time):
 	return Time[0:2]
+
+def make_password_to_save(password):
+	return hashlib.sha256(password.encode()).hexdigest()
 
 def get_data_from_user():
 	name = input('Name: ')
 	work = input('Work: ')
 	Time = __check_num(get_time())
-	return [(name, work, Time)]
+	data = name, work, Time
+	return data
 
-def send_to_database(data):
-	with sql.connect('test.db') as conn:
-		c = conn.cursor()
-		c.execute(f'CREATE TABLE {data[0][0]} (name, work, time)')
-		c.executemany(f"INSERT INTO {data[0][0]} VALUES(?, ?, ?)", data)
-		conn.commit()
+#lets go to json, I'm the best programmer in the world, I LOVE CODING
 
-def print_all():
-	return_data = []
-	with sql.connect('test.db') as conn:
-		c = conn.cursor()
-		c.execute('SELECT name FROM sqlite_master WHERE type="table";')
-		for name in c.fetchall():
-			for row in c.execute(f'SELECT * FROM {name[0]}'):
-				return_data.append(row)
-	return return_data
+def get_json(data):
+	with open('data.json', 'r') as readfile:
+		send_data = json.load(readfile)
+	gusse_table = [table_name for table_name in send_data if table_name == data[0]]
+	if gusse_table == []:
+		send_data[data[0]] = []
+		send_data[data[0]].append({'name': data[1], 'work': data[2]})
+		with open('data.json', 'w') as jfile:
+			json.dump(send_data, jfile)
+	else:
+		send_data[gusse_table[0]].append({'name': data[1], 'work': data[2]})
 
-def send_users_in_they_houre_range(data):
-	for filename in os.listdir("days_dataset"):
-		find_num_from_filename = int(filename.split('.')[0])
-		if find_num_from_filename == int(data[3]):
-			with sql.connect(os.path.join("days_dataset", filename)) as conn:
-				c = conn.cursor()
-				c.executemany(f"INSERT INTO {data[0]} VALUES(?)", data)
-				c.commit()
+def send_request(send_user, name, time_range):
+	with open('data.json', 'r') as read:
+		send_data = json.load(read)
+	gusse_table = [table_name for table_name in send_data if table_name == send_user]
+	if gusse_table != []:
+		send_data[gusse_table[0]].append({'request_user': name, 'time_range': time_range})
+	else:
+		print('user not founded')
+		
 
-def get_data():
-	for filename in os.listdir("days_dataset"):
-		with sql.connect(os.path.join("days_dataset", filename)) as conn:
-			c = conn.cursor()
-			c.execute('SELECT name FROM sqlite_master WHERE type="table";')
-			for name in c.fetchall():
-				for row in c.execute(f"SELECT * FROM {name[0]}"):
-						print(row[:2])
+
+def main():
+	user = get_data_from_user()
+	today = today_date()
+	to = ''
+	to = to+today[:10]+today[19:] 
+	data = (to, user[0], user[1], user[2])
+	get_json(data)
+
+if __name__ == '__main__':
+	main()
