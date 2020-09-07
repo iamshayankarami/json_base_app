04b4a1c76141f1c70c7ad23f07162c901d44b651
Iamshayankarami <ftfsk8203@gmail.com>
2020-09-08 00:38:40 +0430

    some kivy changes

--- app/main.py
+++ app/main.py
@@ -1,44 +1,59 @@
-import kivy
+import kivy, json, os
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
-
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 kivy.require("1.10.1")
 
-class sing_in(GridLayout):
+class singin(GridLayout):
 	def __init__(self, **kwargs):
-		super().__init__(**wargs)
-		self.cols = 2
-		self.add_widget(Label(text="name:"))
-		self.name = TextInput(multiline=False)
-		self.add_widget(self.ip)
+		super().__init__(**kwargs)
+		self.cols, self.return_data = 2, {}
 		
-		self.add_widget(Label(text="username:"))
+		self.add_widget(Label(text='Name: '))
+		self.name = TextInput(multiline=False)
+		self.return_data['name']=self.name
+		self.add_widget(self.name)
+
+		self.add_widget(Label(text='Username: '))
 		self.username = TextInput(multiline=False)
+		self.return_data['username']=self.username
 		self.add_widget(self.username)
 
-		self.add_widget(Label(text="password:"))
-		self.password = TextInput(multiline=False)
+		self.add_widget(Label(text='Password: '))
+		self.password = TextInput(password=True, multiline=False)
+		self.return_data['passeord']=self.password
 		self.add_widget(self.password)
-		
-		self.add_widget(Label(text="work:"))
+
+		self.add_widget(Label(text='Work: '))
 		self.work = TextInput(multiline=False)
+		self.return_data['work']=self.work
 		self.add_widget(self.work)
 
-		self.add_widget(Label(text="timeline:"))
+		self.add_widget(Label(text='Timeline: '))
 		self.timeline = TextInput(multiline=False)
+		self.return_data['timeline']=self.timeline
 		self.add_widget(self.timeline)
 
-		self.add_widget(Label(text="location:"))
+		self.add_widget(Label(text='Location: '))
 		self.location = TextInput(multiline=False)
-		self.add_widget(self.location)
+		self.return_data['location']=self.location
+		self.add_widget(self.timeline)
 
+class login(GridLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 
-class login_page(GridLayout):
-	pass
+class mainPage(GridLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 
-class main_user_page(GridLayout):
-	pass
+class Main(App):
+	def build(self):
+		pass
 
-class 
+if __name__ == '__main__':
+	main_app = Main()
+	main_app.run()
