6ba62a000fd282d3e0a42fad4a01fbf48726d17a
Iamshayankarami <ftfsk8203@gmail.com>
2020-09-08 18:02:10 +0430

    some changes

--- app/main.py
+++ app/main.py
@@ -5,55 +5,57 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
-kivy.require("1.10.1")
 
 class singin(GridLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		self.cols, self.return_data = 2, {}
+		self.cols = 1
 		
 		self.add_widget(Label(text='Name: '))
 		self.name = TextInput(multiline=False)
-		self.return_data['name']=self.name
 		self.add_widget(self.name)
 
 		self.add_widget(Label(text='Username: '))
 		self.username = TextInput(multiline=False)
-		self.return_data['username']=self.username
 		self.add_widget(self.username)
 
 		self.add_widget(Label(text='Password: '))
 		self.password = TextInput(password=True, multiline=False)
-		self.return_data['passeord']=self.password
 		self.add_widget(self.password)
 
 		self.add_widget(Label(text='Work: '))
 		self.work = TextInput(multiline=False)
-		self.return_data['work']=self.work
 		self.add_widget(self.work)
 
 		self.add_widget(Label(text='Timeline: '))
 		self.timeline = TextInput(multiline=False)
-		self.return_data['timeline']=self.timeline
 		self.add_widget(self.timeline)
 
 		self.add_widget(Label(text='Location: '))
 		self.location = TextInput(multiline=False)
-		self.return_data['location']=self.location
-		self.add_widget(self.timeline)
+		self.add_widget(self.location)
 
-class login(GridLayout):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
+		self.Sing_in = Button(text="singin")
+		self.Sing_in.bind(on_press=self.sing_in)
+		self.add_widget(Label())
+		self.add_widget(self.Sing_in)
 
-class mainPage(GridLayout):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
+	def sing_in(self, instance):
+		self.return_data={'name': self.name.text, 'username': self.username.text, 'password': self.password.text, 'work': self.work.text, 'timeline': self.timeline.text, 'location': self.location.text}
+		push_data=self.return_data
+		print(push_data)
+
+#class login(GridLayout):
+#	def __init__(self, **kwargs):
+#		super().__init__(**kwargs)
+
+#class mainPage(GridLayout):
+#	def __init__(self, **kwargs):
+#		super().__init__(**kwargs)
 
 class Main(App):
 	def build(self):
-		pass
+		return singin()
 
 if __name__ == '__main__':
-	main_app = Main()
-	main_app.run()
+	Main().run()
