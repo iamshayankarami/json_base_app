1d739eabb9f00e4b8658b7b7fe072801995ad112
Iamshayankarami <ftfsk8203@gmail.com>
2020-09-09 19:06:43 +0430

    make some changes

--- app/main.py
+++ app/main.py
@@ -6,6 +6,14 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+def check_the_inputs(input_data):
+	for parts in input_data:
+		if input_data[parts] == '':
+			return "ERROR"
+		elif parts == 'timeline':
+			if len(input_data[parts].split('/')) != 3:
+				return "ERROR"
+
 class singin(GridLayout):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
@@ -43,19 +51,39 @@ class singin(GridLayout):
 	def sing_in(self, instance):
 		self.return_data={'name': self.name.text, 'username': self.username.text, 'password': self.password.text, 'work': self.work.text, 'timeline': self.timeline.text, 'location': self.location.text}
 		push_data=self.return_data
-		print(push_data)
+		if check_the_inputs(push_data) == None:
+			print(push_data)
+		else: print(check_the_inputs(push_data))
 
-#class login(GridLayout):
-#	def __init__(self, **kwargs):
-#		super().__init__(**kwargs)
+class login(GridLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.cols=1
+		
+		self.add_widget(Label(text="username: "))
+		self.username = TextInput(multiline=False)
+		self.add_widget(self.username)
 
-#class mainPage(GridLayout):
-#	def __init__(self, **kwargs):
-#		super().__init__(**kwargs)
+		self.add_widget(Label(text="password"))
+		self.password = TextInput(multiline=False, password=True)
+		self.add_widget(self.password)
+
+		self.logiN = Button(text="Login")
+		self.logiN.bind(on_press=self.Login)
+		self.add_widget(Label())
+		self.add_widget(self.logiN)
+
+	def Login(self, instance):
+		self.push_data={'password': self.password.text}
+		print(self.push_data)
+
+class mainPage(GridLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 
 class Main(App):
 	def build(self):
-		return singin()
+		return login()
 
 if __name__ == '__main__':
 	Main().run()
